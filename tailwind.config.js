c4f4a819ac6d97410b9ec514d80d5988a1b0f688
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2023-12-11 00:59:56 +0800

    update on src file to implement daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require("daisyui"),
+    require("tw-elements/dist/plugin.cjs")
+  ],
+  darkMode: "class"
+}
+
