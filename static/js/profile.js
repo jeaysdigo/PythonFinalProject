54c47f97bf2e07f3f4fdefebb6226aeedfc266f9
AG-Padilla <angel.padilla.g@bulsu.edu.ph>
2023-12-12 20:14:43 +0800

    Update edit profile

--- static/js/profile.js
+++
@@ -1,9 +0,0 @@
-var modal = document.getElementById("modal");
-var modalImg = document.getElementById("modal-img");
-function showImage(src) {
-    modal.classList.remove('invisible');
-    modalImg.src = src;
-}
-function closeImage() {
-    modal.classList.add('invisible');
-}
