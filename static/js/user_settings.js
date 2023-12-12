184f139ca5336a1a2c3f5f5421cf35dbafc738c8
AG-Padilla <angel.padilla.g@bulsu.edu.ph>
2023-12-12 15:40:15 +0800

    design updated

--- static/js/user_settings.js
+++ static/js/user_settings.js
@@ -1,13 +1,3 @@
-function openChangeEmail() {
-    document.body.classList.add("overflow-hidden");
-    document.getElementById("changeEmailModal").classList.remove("invisible");
-  }
-
-  function closeChangeEmail() {
-    document.body.classList.remove("overflow-hidden");
-    document.getElementById("changeEmailModal").classList.add("invisible");
-    document.getElementById("newEmail").value = "";
-  }
 
   function openVerifyEmail() {
     document.body.classList.add("overflow-hidden");
@@ -22,27 +12,4 @@ function openChangeEmail() {
     document.getElementById("enterVerificationCode").value = "";
   }
 
-  function openChangePassword() {
-    document.body.classList.add("overflow-hidden");
-    document.getElementById("changePassModal").classList.remove("invisible");
-  }
 
-  function closeChangePassword() {
-    document.body.classList.remove("overflow-hidden");
-    document.getElementById("changePassModal").classList.add("invisible");
-    document.getElementById("oldPass").value = "";
-    document.getElementById("newPass").value = "";
-    document.getElementById("confirmPass").value = "";
-  }
-
-  function openDeleteAccount() {
-    document.body.classList.add("overflow-hidden");
-    document.getElementById("deleteAccountModal").classList.remove("invisible");
-  }
-
-  function closeDeleteAccount() {
-    document.body.classList.remove("overflow-hidden");
-    document.getElementById("deleteAccountModal").classList.add("invisible");
-    document.getElementById("deleteEmail").value = "";
-  }
-  
